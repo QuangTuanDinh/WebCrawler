18310f00a054530f891c8718604321527a2e0fe5
Tuan Dinh <tuandinhsemail@gmail.com>
2018-11-01 02:34:32 -0700

    added output

--- pr1.py
+++ pr1.py
@@ -4,37 +4,42 @@ import re
 from queue import Queue
 from pprint import pprint
 import csv
-import sys
+import timeit
 
 
 def find_urls(html: str):
     v1 = r'href=[\'"]?(?!/)([^\'" >]+)'
     regex = r'a href\s?=\s?[\'"]?(?![\W(mailto:)])([^\'" >]+)'
     for new_url in re.findall(regex, html):
-        if re.search('\.', new_url):
-            if re.search('\.html', new_url) and re.search('/', new_url):
-                url_queue.put(new_url)
-                visited[url].append(new_url)
-            elif not re.search('\.html', new_url) and not re.search('javascript:', new_url):
-                url_queue.put(new_url)
-                visited[url].append(new_url)
+        if new_url not in visited[url]:
+            if re.search('\.', new_url):
+                if re.search('\.html', new_url) and re.search('/', new_url):
+                    url_queue.put(new_url)
+                    visited[url].append(new_url)
+                elif not re.search('\.html', new_url) and not re.search('javascript:', new_url):
+                    url_queue.put(new_url)
+                    visited[url].append(new_url)
 
 
+start = timeit.default_timer()
 visited = {}
 url_queue = Queue(maxsize=0)
 for url in open('urls3.txt', 'r'):
     url = url.strip()
     url_queue.put(url)
 
-while len(visited) <= 75 and url_queue.qsize() != 0:
+while url_queue.empty() is not True:
     url = url_queue.get()
     try:
         if url not in visited:
             req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             page = urllib.request.urlopen(req)
-            visited[url] = []
-            pageText = page.read()
-            find_urls(str(pageText))
+            if len(visited) < 75:
+                visited[url] = []
+                pageText = page.read()
+                find_urls(str(pageText))
+            else:
+                break
 
     except urllib.error.HTTPError as err:
         print(err, url)
@@ -45,7 +50,11 @@ while len(visited) <= 75 and url_queue.qsize() != 0:
     except urllib.error.URLError as err:
         print(err, url)
 
+stop = timeit.default_timer()
+print(stop - start)
+
 with open('output.csv', 'w') as f:
     w = csv.writer(f)
     w.writerow(["Page", "Links"])
     w.writerows(visited.items())
+f.close()
--- pr1_async.py
+++ pr1_async.py
@@ -1,56 +1,119 @@
 import urllib.request
 import urllib.error
 import re
-from multiprocessing import Manager, Process, current_process
+from multiprocessing import Manager, Process, Lock, Value, current_process
 import csv
+from time import sleep
 
 
-# Helper method used to search for new website from the current page's html
-def find_urls(current_url, html: str, shared_queue, shared_visited):
-    v1 = r'href=[\'"]?(?!/)([^\'" >]+)'
+# Helper method used to search for new links from the current page's html, then append them to the shared queue
+def find_urls(html: str, shared_queue):
     regex = r'a href\s?=\s?[\'"]?(?![\W(mailto:)])([^\'" >]+)'
+    new_links = []
     for new_url in re.findall(regex, html):
-        temp = shared_visited[current_url]
-        if re.search('\.', new_url):
-            if re.search('\.html', new_url) and re.search('/', new_url):
-                shared_queue.put(new_url)
-                temp.append(new_url)
-            elif not re.search('\.html', new_url) and not re.search('javascript:', new_url):
-                shared_queue.put(new_url)
-                temp.append(new_url)
-        shared_visited[current_url] = temp
+        if new_url not in new_links:
+            if re.search('\.', new_url):
+                if re.search('\.html', new_url) and re.search('/', new_url):
+                    shared_queue.put(new_url)
+                    new_links.append(new_url)
+                elif not re.search('\.html', new_url) and not re.search('javascript:', new_url):
+                    shared_queue.put(new_url)
+                    new_links.append(new_url)
+    return new_links
 
 
-def crawl(shared_queue, shared_visited):
-    while len(shared_visited) <= 75 and shared_queue.qsize() > 0:
-        url = shared_queue.get()
-        try:
-            if url not in shared_visited:
-                # print(current_process().name, url)
-                req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-                page = urllib.request.urlopen(req)
-                shared_visited[url] = []
-                find_urls(url, str(page.read()), shared_queue, shared_visited)
+# Crawls all the links in the queue if the shared exit flag is 0, exits if 1
+# If the queue is empty, waits around 0.1 seconds before trying again.
+def crawl(shared_lock, shared_exit_flag, shared_queue, shared_visited):
+    while shared_exit_flag.value:
+        if shared_queue.empty() is not True:
+            url = shared_queue.get()
+            try:
+                if url not in shared_visited:
+                    shared_lock.acquire()
+                    if len(shared_visited) < 74:
+                        shared_visited[url] = []
+                        shared_lock.release()
+                        req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+                        page = urllib.request.urlopen(req)
+                        new_links = find_urls(str(page.read()), shared_queue)
+                        shared_visited[url] = new_links
+                    else:
+                        shared_exit_flag.value = 0
+                        shared_lock.release()
+                        break
 
-        except urllib.error.HTTPError as err:
-            print(err, url)
+            except urllib.error.HTTPError as err:
+                print(err, url)
 
-        except ValueError as err:
-            print(err)
+            except ValueError as err:
+                print(err)
 
-        except urllib.error.URLError as err:
-            print(err, url)
+            except urllib.error.URLError as err:
+                print(err, url)
+        else:
+            sleep(0.1)
+
+
+def get_top_link(visited_dict, shared_queue, shared_count, exit_flag):
+    count_dict = {}
+    for v in visited_dict.items():
+        for c in v[1]:
+            if c not in count_dict:
+                count_dict[c] = 1
+            else:
+                count_dict[c] += 1
+    max_links = []
+    for count_item in count_dict.items():
+        if count_item[1] > shared_count.value:
+            max_links = [count_item[0]]
+            shared_count.value = count_item[1]
+        elif count_item[1] == shared_count.value:
+            max_links.append(count_item[0])
+
+    for l in max_links:
+        shared_queue.put(l)
+    exit_flag.value = 0
+
+
+def csv_output(visited_dict, shared_count_queue, count, exit_flag):
+    f = open('output_async.csv', 'w')
+    w = csv.writer(f)
+    w.writerow(["Page", "Links"])
+    w.writerows(visited_dict.items())
+    while exit_flag.value:
+        sleep(0.1)
+    w.writerow(['Top Link(s):' + str(count.value)])
+    while shared_count_queue.empty() is False:
+        w.writerow([shared_count_queue.get()])
+    f.close()
+
+
+def generate_output():
+    count_queue = manager.Queue()
+    count = Value('i', 0)
+    exit_flag = Value('i', 1)
+    p_csv = Process(target=csv_output, args=(visited, count_queue, count, exit_flag))
+    p_count = Process(target=get_top_link, args=(visited, count_queue, count, exit_flag))
+
+    p_count.start()
+    p_csv.start()
+
+    p_count.join()
+    p_csv.join()
 
 
 if __name__ == '__main__':
+    lock = Lock()
     manager = Manager()
-    visited = manager.dict()
     url_queue = manager.Queue()
-    for link in open('urls3.txt', 'r'):
+    visited = manager.dict()
+    exit_flag = Value('i', 1)
+    for link in open('urls6.txt', 'r'):
         link = link.strip()
         url_queue.put(link)
 
-    p = [Process(target=crawl, args=(url_queue, visited)) for i in range(4)]
+    p = [Process(target=crawl, args=(lock, exit_flag, url_queue, visited)) for i in range(8)]
 
     # start child processes
     for each in p:
@@ -60,7 +123,4 @@ if __name__ == '__main__':
     for each in p:
         each.join()
 
-    with open('output_async.csv', 'w') as f:
-        w = csv.writer(f)
-        w.writerow(["Page", "Links"])
-        w.writerows(visited.items())
+    generate_output()
